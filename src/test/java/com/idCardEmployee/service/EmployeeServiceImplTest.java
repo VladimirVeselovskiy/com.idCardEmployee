364f762d8e211614f4ea13bd0f921816dcc48c3a
VladimirVeselovskiy <@>
2023-11-21 11:30:35 +0300

    Exception тесты для сервиса

--- src/test/java/com/idCardEmployee/service/EmployeeServiceImplTest.java
+++ src/test/java/com/idCardEmployee/service/EmployeeServiceImplTest.java
@@ -3,6 +3,7 @@ package com.idCardEmployee.service;
 import com.idCardEmployee.entity.CardAccess;
 import com.idCardEmployee.entity.Employee;
 import com.idCardEmployee.entity.LevelAccess;
+import com.idCardEmployee.exception.EmployeeNotFoundException;
 import com.idCardEmployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,11 +17,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceImplTest {
@@ -76,6 +74,19 @@ public class EmployeeServiceImplTest {
         verify(employeeRepository, times(1)).findById(employeeById);
     }
     @Test
+    void  shouldEmployeeNotFoundException_WhenGetEmployeeById() throws EmployeeNotFoundException{
+        //given
+        int employeeId = 1;
+
+        given(employeeRepository.findById(employeeId)).willReturn(Optional.empty());
+
+        //when
+        assertThrows(EmployeeNotFoundException.class, () -> employeeService.getEmployee(employeeId));
+
+        //then
+        verify(employeeRepository, times(1)).findById(employeeId);
+    }
+    @Test
     void shouldUpdateEmployee(){
         //given
         Employee employee = new Employee(1,"Simons","IT",
@@ -91,8 +102,21 @@ public class EmployeeServiceImplTest {
 
         //then
         assertNotNull(updateEmployee);
+        verify(employeeRepository, times(1)).findById(employee.getId());
         verify(employeeRepository, times(1)).save(updateEmployee);
+    }
+    @Test
+    void shouldEmployeeNotFoundException_WhenUpdateEmployee(){
+        //given
+        Employee employeeId = new Employee();
 
+        given(employeeRepository.findById(employeeId.getId())).willReturn(Optional.empty());
+
+        //when
+        assertThrows(EmployeeNotFoundException.class, () -> employeeService.updateEmployee(employeeId));
+
+        //then
+        verify(employeeRepository, times(1)).findById(employeeId.getId());
     }
     @Test
     void shouldDeleteEmployeeById(){
@@ -103,6 +127,23 @@ public class EmployeeServiceImplTest {
 
         //when
         employeeService.deleteEmployee(employeeId);
+
+        //then
+        verify(employeeRepository, times(1)).deleteById(employeeId);
+    }
+    @Test
+    void shouldEmployeeNotFoundException_WhenDeleteEmployeeById(){
+        //given
+        int employeeId = 1;
+
+        doThrow(EmployeeNotFoundException.class).when(employeeRepository).deleteById(employeeId);
+
+        //when
+        try {
+            employeeService.deleteEmployee(employeeId);
+        }catch (EmployeeNotFoundException e){
+            assertEquals("Employee is not found, id= " + employeeId, e.getMessage());
+        }
 
         //then
         verify(employeeRepository, times(1)).deleteById(employeeId);
