967b370037ef83f46eafa90ff6b727f681cb2e55
VladimirVeselovskiy <@>
2023-11-28 15:23:45 +0300

    Удалил JsonIdentityInfo в CardAccess

--- src/main/java/com/idCardEmployee/entity/CardAccess.java
+++ src/main/java/com/idCardEmployee/entity/CardAccess.java
@@ -1,16 +1,9 @@
 package com.idCardEmployee.entity;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "card_access")
-@JsonIdentityInfo(
-        generator = ObjectIdGenerators.PropertyGenerator.class,
-        property = "id"
-)
 public class CardAccess {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
